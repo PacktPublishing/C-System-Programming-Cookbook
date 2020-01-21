aa1710d1e24bc8146149cab8df4d73195fca347d
Ono Vaticone <onorato@apple.com>
2020-01-21 18:46:44 +0000

    Includes Technical Review chapter 11

--- chapter11/schedAffinity.cpp
+++ chapter11/schedAffinity.cpp
@@ -12,39 +12,15 @@ int main ()
 
    cpu_set_t cpuset;
    CPU_ZERO(&cpuset);
-   int cpu_id = 1;
-   CPU_SET(cpu_id, &cpuset);
-   cpu_id = 3;
+   int cpu_id = 3;
    CPU_SET(cpu_id, &cpuset);
 
-   // sched_setaffinity: This function installs the cpusetsize bytes long affinity mask pointed to
-   // by cpuset for the process or thread with the ID pid. If successful the function returns zero and the scheduler 
-   // will in future take the affinity information into account.
    int set_result = sched_setaffinity(getpid(), sizeof(cpu_set_t), &cpuset);
    if (set_result != 0) 
    {
       std::cerr << "Error on sched_setaffinity" << std::endl;
       std::cout << errno << "-" << strerror(errno) << std::endl;
    }
-
-   // Check what is the actual affinity mask that was assigned to the thread.
-   // sched_getaffinity: This functions stores the CPU affinity mask for the process or thread with the ID pid in 
-   // the cpusetsize bytes long bitmap pointed to by cpuset. If successful, the function always initializes all bits
-   // in the cpu_set_t object and returns zero.
-   // int get_affinity = sched_getaffinity(pid, sizeof(cpu_set_t), &cpuset);
-   // if (get_affinity != 0) 
-   // {
-   //    std::cout << "Error on sched_getaffinity" << std::endl;
-   // }
- 
-   // CPU_ISSET: This macro returns a nonzero value (true) if cpu is a member of the CPU set set, and zero (false) otherwise.
-   // if (CPU_ISSET(cpu_id, &cpuset)) 
-   // {
-   //    std::cout << "Successfully set thread " << pid << " to affinity to CPU: " << cpu_id << std::endl;
-   // } else 
-   // {
-   //    std::cerr << "Failed set thread to affinity to CPU " << std::endl;
-   // }
 
    std::cout << "After sched_setaffinity => ";
    current_affinity();
--- chapter11/schedNice.cpp
+++ chapter11/schedNice.cpp
@@ -5,13 +5,11 @@
 int main ()
 {
    std::cout << "Starting ..." << std::endl;
-   int retCode = nice (-5);
-   if (retCode == -1)
+
+   if (nice (5) == -1)
       std::cout << "nice failed = " << strerror(errno) << std::endl;
    else
       std::cout << "nice value succesfully set = " << std::endl;
-
-   nice (5);
 
    while (1) ;
    std::cout << "End ..." << std::endl;
